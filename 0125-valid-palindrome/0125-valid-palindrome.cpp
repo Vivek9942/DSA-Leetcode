53f978bd48562ffd61b92f23a1b529814e2b1dc1
Vivek Pandey <pandeyvivek9631@gmail.com>
2026-08-25 16:02:58 +0530

    Time: 4 ms (14.13%), Space: 10.9 MB (8%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,43 +1,60 @@
+#include<string>
+#include<iostream>
+#include<vector>
+using namespace std;
 class Solution {
-public:
-    char tolowercase(char ch){
-    if(ch >= 'a' && ch <= 'z'){
-        return ch;
+private:
+    bool valid(char ch) {
+        if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+            return 1;
+        }
+        
+        return 0;
     }
+    
+    char toLowerCase(char ch) {
+    if( (ch >='a' && ch <='z') || (ch >='0' && ch <='9') )
+        return ch;
     else{
-        char temp = ch - 'A' +'a';
+        char temp = ch - 'A' + 'a';
         return temp;
     }
 }
+    bool checkPalindrome(string a) {
+    int s = 0;
+    int e = a.length()-1;
 
-    bool valid(char ch){
-        if(ch>='a' && ch<='z' || ch>='A' && ch<='Z' || ch>='0' && ch<='9'){
-            return true;
+    while(s<=e) {
+        if(a[s] != a[e])
+        {
+            return 0;       
         }
-        return false;
+        else{
+            s++;
+            e--;
+        }
     }
-
-
+    return 1;
+}
+public:
     bool isPalindrome(string s) {
-        int st=0;
-        int end=s.size()-1;
-
-        while(st <= end){
-            if(!valid(s[st])){
-                st++;
-            }
-            else if(!valid(s[end])){
-                end--;
-            }
-            else if(tolowercase(s[st]) != tolowercase(s[end])){
-                return false;
-            }
-            else{
-                st++;
-                end--;
-
+        
+        //faltu character hatado
+        string temp = "";
+        
+        for(int j=0; j<s.length(); j++) {   
+            if(valid(s[j])) {
+                temp.push_back(s[j]);
             }
         }
-        return true;
+        
+        //lowercase me kardo
+        for(int j=0; j<temp.length(); j++) { 
+            temp[j] = toLowerCase(temp[j]);
+        }
+        
+        //check palindrome
+        return checkPalindrome(temp);
+        
     }
 };
